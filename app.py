a2aacd45c26126cfbe3c7d3eb337d687f1ed053e
akarimkesheva <akarimkesheva@ivytech.edu>
2026-09-11 21:54:45 +0500

    8 UI screens and updated routes

--- app.py
+++ app.py
@@ -2,8 +2,58 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/age", methods=["GET", "POST"])
+def age():
+    if request.method == "POST":
+        selected_age = request.form.get("selected_age")
+        
+        print(f"User selected age: {selected_age}")
+    return render_template("age.html")
+
+@app.route("/weight", methods=["GET", "POST"])
+def weight():
+    if request.method == "POST":
+        selected_weight = request.form.get("selected_weight")
+        print(f"User selected weight: {selected_weight}")
+    return render_template("weight.html")
+
+@app.route("/gender", methods=["GET", "POST"])
+def gender():
+    if request.method == "POST":
+        selected_gender = request.form.get("gender")
+        print(f"User selected gender: {selected_gender}")
+    return render_template("gender.html")
+
+@app.route("/height", methods=["GET", "POST"])
+def height():
+    if request.method == "POST":
+        selected_height = request.form.get("selected_height")
+        print(f"User selected height: {selected_height}")
+    return render_template("height.html")
+
+@app.route('/activity', methods=['GET', 'POST'])
+def activity():
+    if request.method == 'POST':
+        
+        pass
+    return render_template('activity.html')
+
+@app.route('/goal', methods=['GET', 'POST'])
+def goal():
+    if request.method == 'POST':
+        # Handle saving the goal data here
+        pass
+    return render_template('goal.html')
+
+@app.route("/profile", methods=["GET", "POST"])
 def profile():
     message = None
 
@@ -22,6 +72,8 @@ def profile():
 
     return render_template("profile.html", message=message)
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
